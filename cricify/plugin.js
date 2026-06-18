43dec66d7e15d0e9a2dee86ab93addaf6f764f0b
Eclipsia <novahaumea@proton.me>
2026-06-18 16:52:45 +0000

    Update plugin.js

--- cricify/plugin.js
+++ cricify/plugin.js
@@ -14,7 +14,7 @@
     const HEADERS = {
         "accept": "*/*",
         "Cache-Control": "no-cache, no-store",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:151.0) Gecko/20100101 Firefox/151.0",
     };
 
     let cachedBaseUrl = null;
